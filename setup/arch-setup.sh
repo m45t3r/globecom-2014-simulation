0aca83cd4ba11649da41012029eb3017bd30bb40
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2014-10-07 11:36:29 -0300

    Cleanup arch-setup.sh script, added related files to .gitignore

--- setup/arch-setup.sh
+++ setup/arch-setup.sh
@@ -1,21 +1,26 @@
+#!/bin/bash
+
+PIP_CACHE='.pipcache'
+
+# source spawns on the current shell and exits, so we need this workaround
+# http://stackoverflow.com/a/13122219
+activate() {
+	source env/bin/activate
+}
+
 # INSTAL DEPENDENCIES
 sudo pacman -Syy
 sudo pacman -S --noconfirm base-devel blas lapack gcc-fortran glpk
 sudo pacman -S --noconfirm python2 python2-pip python2-virtualenv
 
-# FORCE PYTHON 2
-sudo ln -sf /usr/bin/python2 /usr/bin/python
-sudo ln -sf /usr/bin/pip2 /usr/bin/pip
-
 # CREATE VIRTUAL ENV
-virtualenv2 env
-source env/bin/activate
+virtualenv2 env && activate
 
 # PYTHON PIP PACKAGES
-mkdir ~/.pipcache
-source env/bin/activate && pip install --download-cache ~/.pipcache numpy==1.9.0
-source env/bin/activate && pip install --download-cache ~/.pipcache FuncDesigner==0.5402
-source env/bin/activate && pip install --download-cache ~/.pipcache openopt==0.5402
-source env/bin/activate && pip install --download-cache ~/.pipcache matplotlib==1.4.0
-source env/bin/activate && pip install --download-cache ~/.pipcache inspyred==1.0
-source env/bin/activate && export CVXOPT_BUILD_GLPK=1 && pip install --download-cache ~/.pipcache cvxopt==1.1.7
+mkdir "${PIP_CACHE}"
+pip install --download-cache "${PIP_CACHE}" numpy==1.9.0
+pip install --download-cache "${PIP_CACHE}" FuncDesigner==0.5402
+pip install --download-cache "${PIP_CACHE}" openopt==0.5402
+pip install --download-cache "${PIP_CACHE}" matplotlib==1.4.0
+pip install --download-cache "${PIP_CACHE}" inspyred==1.0
+export CVXOPT_BUILD_GLPK=1 && pip install --download-cache "${PIP_CACHE}" cvxopt==1.1.7
